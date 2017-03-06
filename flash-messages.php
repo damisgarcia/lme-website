947de28057054ac27b19c1efdfd30e7b1f1dda72
damisgarcia <damisgarcia@gmail.com>
2017-03-06 11:19:46 -0300

    Repair Declare php method in messages

--- flash-messages.php
+++ flash-messages.php
@@ -1,5 +1,5 @@
 <?php
-  session_start()
+  session_start();
 ?>
 
 <?php if(isset($_SESSION['success']) && !empty($_SESSION['success'])) { ?>
@@ -7,14 +7,14 @@
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <strong>Sua mensagem foi enviada com sucesso.</strong>
   </div>
-<? } ?>
+<?php } ?>
 
 <?php if(isset($_SESSION['danger']) && !empty($_SESSION['danger'])) {?>
   <div id="fail_email" class="alert alert-danger alert-dismissible fixed-alert fade in text-center">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <strong>Não foi possível enviar sua mensagem.</strong>
   </div>
-<? } ?>
+<?php } ?>
 
 <?php
   // Clear old messages
